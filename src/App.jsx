baa6f6e5466be4a61777cf4303873be757ec3900
pdarcy <pdarcy@riseup.net>
2024-05-03 07:50:50 -0400

    finish homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './styles/App.css'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Search from './components/Search'
+import GameCard from './components/GameCard'
+import GenreCard from './components/GenreCard'
+import Home from './pages/Home'
+import About from './pages/About'
 
 const App = () => {
 
   return (
     <div>
-
+      <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </main>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Search from '../components/Search'
+import GameCard from '../components/GameCard'
+import GenreCard from '../components/GenreCard'
+import axios from 'axios'
 
 const Home = () => {
   const [genres, setGenres] = useState([])
@@ -7,33 +11,52 @@ const Home = () => {
   const [searchQuery, setSearchQuery] = useState('')
 
   const getGenres = async () => {
-
+    const response = await axios.get(`https://api.rawg.io/api/genres?key=${import.meta.env.VITE_RAWG_KEY}`)
+    setGenres(response.data.results)
   }
 
   const getSearchResults = async (e) => {
     e.preventDefault()
+    const response = await axios.get(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_RAWG_KEY}&search=${searchQuery}`)
+    setSearchResults(response.data.results);
+    console.log(response.data.results)
+    toggleSearched(true);
+    setSearchQuery('');
   }
 
   const handleChange = (event) => {
-
+    setSearchQuery(event.target.value);
   }
+
+  useEffect(() => {
+    getGenres()
+  }, [])
 
   return (
     <div>
       <div className="search">
         <h2>Search Results</h2>
         <section className="search-results container-grid">
-
+          <Search onChange={handleChange} onSubmit={getSearchResults}/>
         </section>
       </div>
       <div className="genres">
         <h2>Genres</h2>
         <section className="container-grid">
-
+            {
+            genres.map((genre) => (
+                <GenreCard 
+                  name={genre.name}
+                  gameCount={genre.games_count}
+                  image={genre.image_background}
+                />
+                  
+            ))}
         </section>
       </div>
     </div>
   )
 }
+
 
 export default Home
